4aaca4d40b0aeee8166a9b84798734c530971fde
Vener Zaynullin <zaynullin.venya@yandex.ru>
2023-06-28 09:44:21 +0300

    Add host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.64.70', '127.0.0.1', 'localhost', 'lovekitty.ddns.net']
 
 
 # Application definition
